eac0ad9bb6383d2479e8273c442c15c1e2cead63
tamal103 <tamaldx@gmail.com>
2025-06-27 20:17:46 +0600

    test update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Home | Global River Conservation Dashboard</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="{{asset('css/style.css')}}" />A
+  <link rel="stylesheet" href="{{asset('css/style.css')}}" />
 </head>
 <body>
 
